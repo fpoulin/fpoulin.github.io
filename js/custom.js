7be739d155541141ac1278308d4959dd4c6a4c67
Florian Poulin <florian.poulin@gmail.com>
2014-10-27 14:52:20 +0800

    Content, 'More info' toggles

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,22 @@
 $(function(){
+	
 	$('.portfolio-item-thumbnail-wrap').tooltip();
+	
 	$("a[rel^='prettyPhoto']").prettyPhoto({
 		"theme": "dark_square",
 		"social_tools": false
 	});
-});
 
+	$('.moreInfo-tgl').click(function(e){
+		e.preventDefault();
+		var divId = $(this).attr('href');
+		var a = $(this);
+		$(divId).toggle(function(){
+			if ($(this).css('display') == 'none') {
+				a.html('More info');
+			} else {
+				a.html('Less info');
+			}
+		});
+	});
+});
